49b506f9f98e803aea7a4fb4f82ef1d5ab1555d0
Gabriel McColl <gabriel@cloudkingdoms.com>
2021-03-09 22:20:48 +0000

    working to list from Azure blob

--- v1/modules/unittest/versionget/BlobstorageVersionSearch.py
+++ v1/modules/unittest/versionget/BlobstorageVersionSearch.py
@@ -1,10 +1,29 @@
-import requests
+from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import ContainerClient
 import json
-import xml.etree.ElementTree as ET
-uri = 'https://terrreggm.blob.core.windows.net/terrregistryblob/?comp=list'
-#downloaduri = 'https://terrreggm.blob.core.windows.net/terrregistryblob/v1/modules/unittest/versionget/azurerm/1.0.0/local.zip'
-response = requests.get(uri)
-tree = ET.parse(response.content)
-root = tree.getroot()
-for child in root:
-    print(child.tag, child.attrib)
+import re
+uri = "https://terrreggm.blob.core.windows.net/"
+container = "terrregistryblob"
+namespace = "unittest"
+name = "versionget"
+provider = "local"
+name_start = f"v1/modules/{namespace}/{name}/{provider}"
+container_client = ContainerClient(uri, container, credential=None)
+blobs_list = container_client.list_blobs(name_starts_with=name_start)
+x = '''
+    {
+        "modules": [
+        {
+                "versions": [
+                ]
+        }
+        ]
+    }
+    '''
+y =  json.loads(x)  #turn string above json python object
+for module in y['modules']:
+    for blob in blobs_list:
+        ver = (re.search(r'[0-9].[0-9].[0-9]', blob.name).group())
+        module['versions'].append({'version' : ver}) #dig in and loop into versions
+print(json.dumps(y))
+# return  json.dumps(y)
