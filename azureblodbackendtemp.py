91084bd7d10ce6ebfb4b63725136873e9b6a29a7
Gabriel McColl <gabriel@cloudkingdoms.com>
2021-03-11 22:51:33 +0000

    sas generator working

--- azureblodbackendtemp.py
+++ azureblodbackendtemp.py
@@ -4,7 +4,7 @@ import versiongetter
 import shutil
 import os
 import tempfile #1
-from test import BlobDownloadAuthN,BlobSASUri
+from test import BlobDownloadAuthN
 from azure.storage.blob import BlobClient
 from azure.storage.blob import ContainerClient
 from azure.identity import DefaultAzureCredential
@@ -42,5 +42,24 @@ def downloadversion(namespace, name,provider,version):
 #need to actually send the file
 @app.route('/v1/modules/<namespace>/<name>/<provider>/<version>/local.zip', methods=['GET'])
 def downloadfile(namespace, name,provider,version):
-    sasuri = BlobSASUri(azblobstoragehost,'terrreggm',azcontainer, namespace,name,provider,version)
-    return redirect(sasuri)
+    bloburl = f'{azblobstoragehost}/{azcontainer}/v1/modules/{namespace}/{name}/{provider}/{version}/local.zip'
+    token_credential = DefaultAzureCredential()
+    blob_client = BlobClient.from_blob_url(bloburl, credential=token_credential)  
+    download_stream = blob_client.download_blob()
+    # create a temporary directory using the context manager
+    #f = tempfile.TemporaryDirectory(dir = "temp")
+    f = tempfile.mkdtemp(dir = "temp") #this one works
+    with open(f'{f}/local.zip', "wb") as my_blob:
+        download_stream = blob_client.download_blob()
+        my_blob.write(download_stream.readall())
+    return send_file(f'{my_blob.name}')
+     
+
+#Get Versions
+@app.route('/v1/modules/cleartemp', methods=['GET'])
+def cleartemp():
+    dir = 'temp'
+    shutil.rmtree(dir)
+    os.makedirs(dir)
+    open(f'{dir}/placeholder.txt',"w+")
+    return f'cleared temp'
--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 from azure.storage.blob import BlobClient,ContainerClient,BlobServiceClient,generate_blob_sas,BlobSasPermissions
 from azure.identity import DefaultAzureCredential
 from datetime import datetime,timezone,timedelta
-
 import tempfile
-import os
-import shutil
+
 
 def BlobDownloadAuthN(host,container,namespace, name,provider,version):
     # blobpath = f'/v1/modules/{namespace}/{name}/{provider}/{version}/local.zip'
@@ -13,21 +11,18 @@ def BlobDownloadAuthN(host,container,namespace, name,provider,version):
     blob_client = BlobClient.from_blob_url(bloburl, credential=token_credential)  
     download_stream = blob_client.download_blob()
     return download_stream
-#remove the temp dir
-azblobstoragehost = "https://terrreggm.blob.core.windows.net"
-azblobaccount = 'terrreggm'
-azcontainer = "terrregistryblob"
-# BlobDownloadAuthN(azblobstoragehost,azcontainer,'unittest','versionget','azurerm','1.0.0')
-token_credential = DefaultAzureCredential()
-blob_service = BlobServiceClient(azblobstoragehost, credential=token_credential)
-start_utc = datetime.now(timezone.utc) - timedelta(hours=1)
-end_utc = datetime.now(timezone.utc) + timedelta(hours=1)
-u_d_key= blob_service.get_user_delegation_key(start_utc, end_utc,timeout = 20)
-#blob = f'local.zip'
-blob = r'v1/modules/unittest/versionget/azurerm/1.2.5/local.zip'
-bloburl = f'{azblobstoragehost}/{azcontainer}/{blob}'
 
+def BlobSASUri(host,account,container,namespace, name,provider,version):
+    blob = f'v1/modules/{namespace}/{name}/{provider}/{version}/local.zip'
+    bloburl = f'{host}/{container}/{blob}'
+    azblobaccount = '' #need a way to pull this from the host
+    token_credential = DefaultAzureCredential()
+    #create a blob service with either the local dev environment variables or system managed identity
+    blob_service = BlobServiceClient(host, credential=token_credential) 
+    start_utc = datetime.now(timezone.utc) - timedelta(minutes=5)
+    end_utc = datetime.now(timezone.utc) + timedelta(minutes=5)
+    u_d_key= blob_service.get_user_delegation_key(start_utc, end_utc,timeout = 20)
+    sasPerm = BlobSasPermissions(read=True,tag=False)
+    blobsas = generate_blob_sas(account, container, blob, user_delegation_key=u_d_key, permission=sasPerm, expiry=end_utc, start=start_utc)
+    return f'{bloburl}?{blobsas}'
 
-sasPerm = BlobSasPermissions(read=True,tag=False)
-blobsas = generate_blob_sas(azblobaccount, azcontainer, blob, user_delegation_key=u_d_key, permission=sasPerm, expiry=end_utc, start=start_utc)
-print(f'{bloburl}?{blobsas}')
