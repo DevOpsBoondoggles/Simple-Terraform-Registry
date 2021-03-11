a24ec2fde7616d328fe899912be904faad786efb
Gabriel McColl <gabriel@cloudkingdoms.com>
2021-03-11 22:03:09 +0000

    generate a SAS and user delegation key. url works

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from azure.storage.blob import BlobClient
-from azure.storage.blob import ContainerClient
+from azure.storage.blob import BlobClient,ContainerClient,BlobServiceClient,generate_blob_sas,BlobSasPermissions
 from azure.identity import DefaultAzureCredential
+from datetime import datetime,timezone,timedelta
+
 import tempfile
 import os
 import shutil
@@ -13,6 +14,20 @@ def BlobDownloadAuthN(host,container,namespace, name,provider,version):
     download_stream = blob_client.download_blob()
     return download_stream
 #remove the temp dir
-# azblobstoragehost = "https://terrreggm.blob.core.windows.net"
-# azcontainer = "terrregistryblob"
+azblobstoragehost = "https://terrreggm.blob.core.windows.net"
+azblobaccount = 'terrreggm'
+azcontainer = "terrregistryblob"
 # BlobDownloadAuthN(azblobstoragehost,azcontainer,'unittest','versionget','azurerm','1.0.0')
+token_credential = DefaultAzureCredential()
+blob_service = BlobServiceClient(azblobstoragehost, credential=token_credential)
+start_utc = datetime.now(timezone.utc) - timedelta(hours=1)
+end_utc = datetime.now(timezone.utc) + timedelta(hours=1)
+u_d_key= blob_service.get_user_delegation_key(start_utc, end_utc,timeout = 20)
+#blob = f'local.zip'
+blob = r'v1/modules/unittest/versionget/azurerm/1.2.5/local.zip'
+bloburl = f'{azblobstoragehost}/{azcontainer}/{blob}'
+
+
+sasPerm = BlobSasPermissions(read=True,tag=False)
+blobsas = generate_blob_sas(azblobaccount, azcontainer, blob, user_delegation_key=u_d_key, permission=sasPerm, expiry=end_utc, start=start_utc)
+print(f'{bloburl}?{blobsas}')
