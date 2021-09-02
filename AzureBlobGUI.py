8160218d38861ad0243958866d884c7716aa7d6d
Gabriel McColl <gabriel@cloudkingdoms.com>
2021-09-02 20:43:14 +0100

    works to render from azure blob

--- AzureBlobGUI.py
+++ AzureBlobGUI.py
@@ -1,7 +1,7 @@
+
 from flask import Flask,render_template
 import os
-import json
-from flask_misaka import markdown,Misaka
+from flask_misaka import Misaka
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import ContainerClient
 
@@ -13,11 +13,22 @@ azblobaccountname = os.environ.get("AZBLOBACCOUNTNAME") #blobaccount
 azblobstoragehost = os.environ.get("AZBLOBSTORAGEHOST") #the full url "https://blobaccount.blob.core.windows.net"
 azcontainer = os.environ.get("AZCONTAINER") #container where the modules are
 
-def ModuleslistGui(host,container):   
+def RootBlobsList(host,container,filepath):   
     token_credential = DefaultAzureCredential()
-    filepath = f"v1/modules/"
     container_client = ContainerClient(host, container, credential=token_credential)
     blobs_list = container_client.list_blobs(name_starts_with=filepath) #
+    return blobs_list
+
+
+
+def GetBlobContent(host,container,filepath):   
+    token_credential = DefaultAzureCredential()
+    container_client = ContainerClient(host, container, credential=token_credential)
+    blob_client = container_client.get_blob_client(filepath)
+    stream = blob_client.download_blob()
+    return stream.content_as_text()
+
+def NamespaceBlobList(blobs_list,filepath):
     x = []
     start = len(filepath) #get the end of the standard path. start of the module name
     #e.g. 'v1/modules/unittest/versionget/azurerm/1.0.0/local.zip'
@@ -29,11 +40,45 @@ def ModuleslistGui(host,container):
         word = stringback[0:end] #go from start of stripped string to the first / 'unittest'
         x.append(word) #dig in and loop into versions
     unique = list(dict.fromkeys(x))#this bit basically makes a dictionary to get only unique values and then converts back to a list
-    return render_template('index.html',modules=unique, filepath=filepath)
-
+    return unique
 
 @app.route('/') # to list all the module namespaces 
 def index():
-   # filepath = f'./v1/modules/'
-    namespaces = ModuleslistGui(azblobstoragehost,azcontainer)
-    return f'{namespaces}'
+    filepath = f'v1/modules/'
+    rootblobs = RootBlobsList(azblobstoragehost,azcontainer,filepath)
+    namespaces = NamespaceBlobList(rootblobs,filepath)
+    return render_template('index.html',modules=namespaces, filepath=filepath)
+    
+
+@app.route('/v1/modules/<namespace>/', methods=['GET']) # list all the modules in a namespace
+def modulenamelist(namespace):
+    basefilepath = f'v1/modules/'
+    filepath = f'{basefilepath}{namespace}/'
+    rootblobs = RootBlobsList(azblobstoragehost,azcontainer,filepath)
+    namespaces = NamespaceBlobList(rootblobs,filepath)
+    return render_template('namespace.html',modules=namespaces, filepath=basefilepath)
+
+@app.route('/v1/modules/<namespace>/<name>/', methods=['GET']) # list all the modules in a namespace
+def providerlist(namespace,name):
+    basefilepath = f'v1/modules/'
+    filepath = f'{basefilepath}{namespace}/{name}/'
+    rootblobs = RootBlobsList(azblobstoragehost,azcontainer,filepath)
+    namespaces = NamespaceBlobList(rootblobs,filepath)
+    return render_template('namespace.html',modules=namespaces, filepath=basefilepath)
+
+@app.route('/v1/modules/<namespace>/<name>/<provider>/', methods=['GET']) # list all the modules in a namespace
+def versionlist(namespace,name,provider):
+    basefilepath = f'v1/modules/'
+    filepath = f'{basefilepath}{namespace}/{name}/{provider}/'
+    rootblobs = RootBlobsList(azblobstoragehost,azcontainer,filepath)
+    namespaces = NamespaceBlobList(rootblobs,filepath)
+    return render_template('namespace.html',modules=namespaces, filepath=basefilepath)
+
+@app.route('/v1/modules/<namespace>/<name>/<provider>/<version>/', methods=['GET'])
+def load_readme(namespace, name,provider,version):
+    filepath = f'v1/modules/{namespace}/{name}/{provider}/{version}'
+    readmecontent = GetBlobContent(azblobstoragehost,azcontainer,f'{filepath}/readme.md')
+    # with open(f'{filepath}/readme.md', 'r') as f:
+    #     content = f.read()
+    return render_template("readme.html",text=readmecontent, title=f'Readme for {namespace}/{name}/{provider}/{version}')
+  
